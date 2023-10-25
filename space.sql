e3c8984bedb2b582ddc8d2286a3052b28870dc50
BOWMAN-03 <40440313+BOWMAN-03@users.noreply.github.com>
2023-10-25 14:38:37 -0400

    space.sql

--- space.sql
+++ space.sql
@@ -28,7 +28,7 @@ select top 2 sum(clist.years_of_service) as Years_Served, clist.state as State, 
 	+ isnull(stage1,0)) as 'total score' 
 from cte_scores
 full outer join clist
-on clist.id = cte_scores.id
+	on clist.id = cte_scores.id
 group by name, state
 order by 'total score' desc;
 
@@ -54,31 +54,32 @@ group by service;
 
 	-- 4. revision that splits the #4 query into score ranges with counts.. uses triple cte to refine 3 columns into ranges and seperates them by gender
 with cte_scores as
-(select isnull(sum(stage_3),0) as 'stage1', isnull(sum(stage_2),0) as 'stage2', isnull(sum(stage_1),0) as 'stage3', cscore.id, gender as 'gender'
+(select isnull(sum(stage_3),0) as 'stage1', isnull(sum(stage_2),0) as 'stage2',
+	isnull(sum(stage_1),0) as 'stage3', cscore.id, gender as 'gender'
 from Cscore
 full outer join Clist
-on cscore.id = clist.id
+	on cscore.id = clist.id
 group by cscore.id, gender),
 cte_scores2 as
 (select id, gender, sum(stage1 + stage2 + stage3) as totscore from cte_scores group by gender, id),
 cte_scores3 as (select gender, case 
 	when totscore between 0 and 49 then '0-49'
 	when totscore between 50 and 99 then '50-99'
-		when totscore between 100 and 149 then '100-149'
-			when totscore between 150 and 199 then '150-199'
-			when totscore between 200 and 249 then '200-249'
-				when totscore between 250 and 300 then '250-300'
-				end as  Score from cte_scores2)
-	select gender, score, count(score) as count from cte_scores3
-	group by score, gender;
+	when totscore between 100 and 149 then '100-149'
+	when totscore between 150 and 199 then '150-199'
+	when totscore between 200 and 249 then '200-249'
+	when totscore between 250 and 300 then '250-300'
+		end as  Score from cte_scores2)
+select gender, score, count(score) as count from cte_scores3
+group by score, gender;
 
 	--5. get years of services, discluding non serving members
-		Select avg(years_of_service*1.00) as years_served
+Select avg(years_of_service*1.00) as years_served
 from clist
 where years_of_service > 0;
 
 	-- 6. get average age, total candidates, gender distribution count
-	Select count(id) as candidates, avg(age) as avg_age, 
+Select count(id) as candidates, avg(age) as avg_age, 
     sum(case gender when 'Male' then 1 else 0 end) as Male,
     sum(case gender when 'Female' then 1 else 0 end) as Female
 from clist;
@@ -130,72 +131,71 @@ order by systems_tests desc;
 
 
 	-- preview 3 tables joined, noted duplicate columns
-	select top 3 *
-	from expend
-	full outer join fund
+select top 3 *
+from expend
+full outer join fund
 	on expend.week = fund.week
-	full outer join vessel
+full outer join vessel
 	on vessel.week = expend.week;
 
 
 	-- 1. cost, funds, and runnings of each
-	select expend.week, expend.start_of_week, (equipment+expend.wages+medical+food+materials+vessel.wages) as cost, sum (equipment+expend.wages+medical+food+materials+vessel.wages) over ( 
-         order by expend.week) as running_cost, sum(government+other) over (order by expend.week) as running_funded
- 			from expend
-	full join fund
+select expend.week, expend.start_of_week, (equipment+expend.wages+medical+food+materials+vessel.wages) as cost, sum (equipment+expend.wages+medical+food+materials+vessel.wages) over ( 
+order by expend.week) as running_cost, sum(government+other) over (order by expend.week) as running_funded
+from expend
+full join fund
 	on expend.week = fund.week
-	full join vessel
+full join vessel
 	on vessel.week = expend.week;
 
 
 	-- 2. funds remaining
-	select ( sum(government+other) - sum(equipment+expend.wages+medical+food+materials+vessel.wages)) as remaining
-	 			from expend
-	full join fund
+select ( sum(government+other) - sum(equipment+expend.wages+medical+food+materials+vessel.wages)) as remaining
+from expend
+full join fund
 	on expend.week = fund.week
-	full join vessel
+full join vessel
 	on vessel.week = expend.week;
 
-
 	-- 3. select source of expenditures and with their percentage of the total costs
-	with cte_ex as (select 
-	sum(equipment) as equipment, sum(expend.wages) as staffing, sum(vessel.wages) as engineers, sum(materials) as material, sum(food) as food, sum(medical) as medical, sum(equipment+expend.wages+medical+food+materials+vessel.wages) as total
-	from expend
-	full join fund
+with cte_ex as (select 
+sum(equipment) as equipment, sum(expend.wages) as staffing, sum(vessel.wages) as engineers, sum(materials) as material, sum(food) as food, sum(medical) as medical, sum(equipment+expend.wages+medical+food+materials+vessel.wages) as total
+from expend
+full join fund
 	on expend.week = fund.week
-	full join vessel
+full join vessel
 	on vessel.week = expend.week)
-	select equipment, 100.00*equipment/total as equipment, staffing, 100.00*staffing/total as staffing, engineers, 100.00*engineers/total as engineers, food, 100.00*food/total as food, medical, 100.00*medical/total as medical, material, 100.00*equipment/total as material
-	from cte_ex;
+select equipment, 100.00*equipment/total as equipment, staffing, 100.00*staffing/total as staffing, engineers, 100.00*engineers/total as engineers, food, 100.00*food/total as food, medical, 100.00*medical/total as medical, material, 100.00*equipment/total as material
+from cte_ex;
 
 
 	-- 4. find day with biggest donation (disclude week 1 on account of it being a starting allowance that would not be a weekly fund, but would be considered the initial funds)
-	select top 1 government+other as total_funding, week, start_of_week
-	from fund
-			where week <> 'week 01'
-	order by total_funding desc;
+select top 1 government+other as total_funding, week, start_of_week
+from fund
+where week <> 'week 01'
+order by total_funding desc;
 
 
 	-- 5. weeks "above water" (cum. funds are more than cum. cost)
-		with CTE_water as (select expend.week, expend.start_of_week, sum (equipment+expend.wages+medical+food+materials+vessel.wages) over ( 
-         order by expend.week) as running_cost, sum(government+other) over (order by expend.week) as running_funded
- 			from expend
-	full join fund
+with CTE_water as (select expend.week, expend.start_of_week, sum (equipment+expend.wages+medical+food+materials+vessel.wages) over ( 
+order by expend.week) as running_cost, sum(government+other) over (order by expend.week) as running_funded
+from expend
+full join fund
 	on expend.week = fund.week
-	full join vessel
+full join vessel
 	on vessel.week = expend.week)
-	select case
-		when running_funded > running_cost then 'pass'
-		when running_funded < running_cost then 'fail'
+select case
+	when running_funded > running_cost then 'pass'
+	when running_funded < running_cost then 'fail'
 		end as 'y'
-	from CTE_water;
+from CTE_water;
 
 	-- 6. govt/other funding
 with cte_funds as (
 	select sum(government) as gov, sum(other) as oth, sum(other+government) as tot
 	from fund)
-	select gov as government_total, 100.00*(1.00*gov/tot) as government_percent, oth as other_total, 100.00*oth/tot as other_percent
-	from cte_funds;
+select gov as government_total, 100.00*(1.00*gov/tot) as government_percent, oth as other_total, 100.00*oth/tot as other_percent
+from cte_funds;
 
 	---- 7. accidental repeat of #4... this uses slightly different query to achieve same result
 	--select sum(government+other), start_of_week
